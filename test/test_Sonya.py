8293657e755f369bc8407917db3648b903c89ac4
Sonya-truba <sonya.truba@gmail.com>
2025-09-18 20:48:39 +0300

    Задание на воркшоп. Соня

---
+++ test/test_Sonya.py
@@ -0,0 +1,11 @@
+import requests
+
+def test_task_Sonya():
+
+    body = {"title": "task_Sonya", "completed": False}
+    response = requests.post("https://todo-app-sky.herokuapp.com/", json=body)
+    id = response.json()["id"]
+    requests.delete(f'https://todo-app-sky.herokuapp.com/{id}')
+    response = requests.get(f'https://todo-app-sky.herokuapp.com/{id}')
+
+    assert response.status_code == 404
